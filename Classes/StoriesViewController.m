7d2f495000d28da4090ff2c531f9c5fc3f0b381b
Alex Wayne <alex@beautifulpixel.com>
2010-03-24 19:05:34 -0700

    Story iPad overhaul.

--- Classes/StoriesViewController.m
+++ Classes/StoriesViewController.m
@@ -1,9 +1,9 @@
 //
-//  RootViewController.m
-//  LatestChatty2
+//    RootViewController.m
+//    LatestChatty2
 //
-//  Created by Alex Wayne on 3/16/09.
-//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//    Created by Alex Wayne on 3/16/09.
+//    Copyright __MyCompanyName__ 2009. All rights reserved.
 //
 
 #import "StoriesViewController.h"
@@ -15,123 +15,128 @@
 @synthesize stories;
 
 - (id)initWithNib {
-  if (self = [super initWithNib]) {
-      self.title = @"Stories";
-  }
-  return self;
+    if (self = [super initWithNib]) {
+            self.title = @"Stories";
+    }
+    return self;
 }
 
 - (id)initWithStateDictionary:(NSDictionary *)dictionary {
-  [self init];
-  self.stories = [dictionary objectForKey:@"stories"];
-  return self;
+    [self init];
+    self.stories = [dictionary objectForKey:@"stories"];
+    return self;
 }
 
 - (NSDictionary *)stateDictionary {
-  return [NSDictionary dictionaryWithObjectsAndKeys:@"Stories", @"type",
-                                                    stories, @"stories", nil];
+    return [NSDictionary dictionaryWithObjectsAndKeys:@"Stories", @"type",
+                                                                                                        stories, @"stories", nil];
 }
 
 
 - (void)viewDidLoad {
-  [super viewDidLoad];
-  
-  if (stories == nil || [stories count] == 0) [self refresh:self];
-  
-  UIBarButtonItem *latestChattyButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"ChatIcon.24.png"]
-                                                                         style:UIBarButtonItemStyleDone
-                                                                        target:self
-                                                                        action:@selector(tappedLatestChattyButton)];
-  self.navigationItem.rightBarButtonItem = latestChattyButton;
-  [latestChattyButton release];
+    [super viewDidLoad];
+    
+    if (stories == nil || [stories count] == 0) [self refresh:self];
+    
+    UIBarButtonItem *latestChattyButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"ChatIcon.24.png"]
+                                                                                                                                                 style:UIBarButtonItemStyleDone
+                                                                                                                                                target:self
+                                                                                                                                                action:@selector(tappedLatestChattyButton)];
+    self.navigationItem.rightBarButtonItem = latestChattyButton;
+    [latestChattyButton release];
 }
 
 - (IBAction)refresh:(id)sender {
-  [super refresh:sender];
-  loader = [[Story findAllWithDelegate:self] retain];
+    [super refresh:sender];
+    loader = [[Story findAllWithDelegate:self] retain];
 }
 
 
 - (void)didFinishLoadingAllModels:(NSArray *)models otherData:(id)otherData {
-  self.stories = models;
-  [loader release];
-  loader = nil;
-  [super didFinishLoadingAllModels:models otherData:otherData];
+    self.stories = models;
+    [loader release];
+    loader = nil;
+    [super didFinishLoadingAllModels:models otherData:otherData];
 }
 
 - (void)didReceiveMemoryWarning {
-  [super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
-  // Release anything that's not essential, such as cached data
+    [super didReceiveMemoryWarning]; // Releases the view if it doesn't have a superview
+    // Release anything that's not essential, such as cached data
 }
 
 #pragma mark Shake Handler
 
 // FIXME: This never gets called
 //- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event {
-//  NSLog(@"Shook!");
-//  if (motion == UIEventSubtypeMotionShake) [self refresh:self];
+//    NSLog(@"Shook!");
+//    if (motion == UIEventSubtypeMotionShake) [self refresh:self];
 //}
 
 #pragma mark Table view methods
 
 // Customize the number of rows in the table view.
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-  NSInteger count = 0;
-  if (self.stories) count = [self.stories count];
-  return count;
+    NSInteger count = 0;
+    if (self.stories) count = [self.stories count];
+    return count;
 }
 
 
 // Customize the appearance of table view cells.
 - (UITableViewCell *)tableView:(UITableView *)aTableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-  static NSString *CellIdentifier = @"StoryCell";
-  
-  StoryCell *cell = (StoryCell *)[aTableView dequeueReusableCellWithIdentifier:CellIdentifier];
-  if (cell == nil) {
-    cell = [[[StoryCell alloc] init] autorelease];
-    [cell.chattyButton addTarget:self action:@selector(tappedChattyButton:) forControlEvents:UIControlEventTouchUpInside];
-  }
-  
-  // Set the story
-  cell.story = [stories objectAtIndex:indexPath.row];
+    static NSString *CellIdentifier = @"StoryCell";
+    
+    StoryCell *cell = (StoryCell *)[aTableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (cell == nil) {
+        cell = [[[StoryCell alloc] init] autorelease];
+        [cell.chattyButton addTarget:self action:@selector(tappedChattyButton:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    
+    // Set the story
+    cell.story = [stories objectAtIndex:indexPath.row];
 
-  return (UITableViewCell *)cell;
+    return (UITableViewCell *)cell;
 }
 
 //- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-//  return [StoryCell cellHeight];
+//    return [StoryCell cellHeight];
 //}
 
 
 #pragma mark Actions
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-  Story *story = [stories objectAtIndex:indexPath.row];
-  StoryViewController *viewController = [[StoryViewController alloc] initWithStoryId:story.modelId];
-  [self.navigationController pushViewController:viewController animated:YES];
-  [viewController release];
+    Story *story = [stories objectAtIndex:indexPath.row];
+    StoryViewController *viewController = [[[StoryViewController alloc] initWithStoryId:story.modelId] autorelease];
+    
+    if ([[LatestChatty2AppDelegate delegate] isPadDevice]) {
+        [LatestChatty2AppDelegate delegate].contentNavigationController.viewControllers = [NSArray arrayWithObject:viewController];
+    } else {
+        [self.navigationController pushViewController:viewController animated:YES];
+    }
 }
 
 - (IBAction)tappedChattyButton:(id)sender {
-  NSIndexPath *indexPath;
-  for (StoryCell *cell in [self.tableView visibleCells]) {
-    if (cell.chattyButton == sender)
-      indexPath = [self.tableView indexPathForCell:cell];
-  }
-  
-  Story *story = [stories objectAtIndex:indexPath.row];
-  UIViewController *viewController = [ChattyViewController chattyControllerWithStoryId:story.modelId];
-  [self.navigationController pushViewController:viewController animated:YES];
+    NSIndexPath *indexPath;
+    for (StoryCell *cell in [self.tableView visibleCells]) {
+        if (cell.chattyButton == sender) {
+            indexPath = [self.tableView indexPathForCell:cell];
+        }
+    }
+    
+    Story *story = [stories objectAtIndex:indexPath.row];
+    UIViewController *viewController = [ChattyViewController chattyControllerWithStoryId:story.modelId];
+    [self.navigationController pushViewController:viewController animated:YES];
 }
 
 - (IBAction)tappedLatestChattyButton {
-  ChattyViewController *viewController = [ChattyViewController chattyControllerWithLatest];
-  [self.navigationController pushViewController:viewController animated:YES];
+    ChattyViewController *viewController = [ChattyViewController chattyControllerWithLatest];
+    [self.navigationController pushViewController:viewController animated:YES];
 }
 
 - (void)dealloc {
-  self.stories = nil;
-  [super dealloc];
+    self.stories = nil;
+    [super dealloc];
 }
 
 
--- Classes/StoryViewController.m
+++ Classes/StoryViewController.m
@@ -1,12 +1,13 @@
 //
-//  StoryViewController.m
-//  LatestChatty2
+//    StoryViewController.m
+//    LatestChatty2
 //
-//  Created by Alex Wayne on 3/18/09.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//    Created by Alex Wayne on 3/18/09.
+//    Copyright 2009 __MyCompanyName__. All rights reserved.
 //
 
 #import "StoryViewController.h"
+#import "LatestChatty2AppDelegate.h"
 
 
 @implementation StoryViewController
@@ -14,113 +15,115 @@
 @synthesize story;
 
 - (id)initWithStoryId:(NSUInteger)aStoryId {
-  self = [super initWithNib];
-  storyId = aStoryId;
-  self.title = @"Loading...";
-  return self;
+    self = [super initWithNib];
+    storyId = aStoryId;
+    self.title = @"Loading...";
+    return self;
 }
 
 - (id)initWithStory:(Story *)aStory {
-  self = [self initWithNibName:@"StoryViewController" bundle:nil];
-  self.story = aStory;
-  self.title = self.story.title;
-  return self;
+    self = [self initWithNibName:@"StoryViewController" bundle:nil];
+    self.story = aStory;
+    self.title = self.story.title;
+    return self;
 }
 
 - (id)initWithStateDictionary:(NSDictionary *)dictionary {
-  return [self initWithStory:[dictionary objectForKey:@"story"]];
+    return [self initWithStory:[dictionary objectForKey:@"story"]];
 }
 
 - (NSDictionary *)stateDictionary {
-  return [NSDictionary dictionaryWithObjectsAndKeys:@"Story", @"type",
-                                                    story,    @"story", nil];
+    return [NSDictionary dictionaryWithObjectsAndKeys:@"Story", @"type",
+                                                                                                        story,        @"story", nil];
 }
 
 - (void)didFinishLoadingModel:(id)model otherData:(id)otherData {
-  self.story = model;
+    self.story = model;
 	[storyLoader release];
-  //[story release];
-  storyLoader = nil;
-  [self displayStory];
+    //[story release];
+    storyLoader = nil;
+    [self displayStory];
 }
 
 - (void)viewDidLoad {
-  [super viewDidLoad];
-  
-  UIBarButtonItem *chattyButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"ChatIcon.24.png"]
-                                                                   style:UIBarButtonItemStylePlain
-                                                                  target:self
-                                                                  action:@selector(loadChatty)];
+    [super viewDidLoad];
+    
+    UIBarButtonItem *chattyButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"ChatIcon.24.png"]
+                                                                                                                                     style:UIBarButtonItemStylePlain
+                                                                                                                                    target:self
+                                                                                                                                    action:@selector(loadChatty)];
 	self.navigationItem.rightBarButtonItem = chattyButton;
-  [chattyButton release];
-  
-  // Load story
-  storyLoader = [[Story findById:storyId delegate:self] retain];
-  
-  // Load blank page while we wait
-  NSString *baseUrlString = [NSString stringWithFormat:@"http://shacknews.com/onearticle.x/%i", story.modelId];
-  StringTemplate *htmlTemplate = [[StringTemplate alloc] initWithTemplateName:@"Story.html"];
-  NSString *stylesheet = [NSString stringWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Stylesheet.css" ofType:nil] usedEncoding:nil error:nil];
-  [htmlTemplate setString:stylesheet forKey:@"stylesheet"];
-  [htmlTemplate setString:@"" forKey:@"date"];
-  [htmlTemplate setString:@"" forKey:@"storyId"];
-  [htmlTemplate setString:@"" forKey:@"content"];
-  [htmlTemplate setString:@"" forKey:@"storyTitle"];
-  
-  [content loadHTMLString:htmlTemplate.result baseURL:[NSURL URLWithString:baseUrlString]];
-  [htmlTemplate release];    
+    [chattyButton release];
+    
+    // Load story
+    storyLoader = [[Story findById:storyId delegate:self] retain];
+    
+    // Load blank page while we wait
+    NSString *baseUrlString = [NSString stringWithFormat:@"http://shacknews.com/onearticle.x/%i", story.modelId];
+    StringTemplate *htmlTemplate = [[StringTemplate alloc] initWithTemplateName:@"Story.html"];
+    NSString *stylesheet = [NSString stringWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Stylesheet.css" ofType:nil] usedEncoding:nil error:nil];
+    [htmlTemplate setString:stylesheet forKey:@"stylesheet"];
+    [htmlTemplate setString:@"" forKey:@"date"];
+    [htmlTemplate setString:@"" forKey:@"storyId"];
+    [htmlTemplate setString:@"" forKey:@"content"];
+    [htmlTemplate setString:@"" forKey:@"storyTitle"];
+    
+    [content loadHTMLString:htmlTemplate.result baseURL:[NSURL URLWithString:baseUrlString]];
+    [htmlTemplate release];        
 }
 
 - (void)displayStory {
-  self.title = story.title;
-  
-  // Load up web view content
-  NSString *baseUrlString = [NSString stringWithFormat:@"http://shacknews.com/onearticle.x/%i", story.modelId];
-  
-  StringTemplate *htmlTemplate = [[StringTemplate alloc] initWithTemplateName:@"Story.html"];
-  
-  NSString *stylesheet = [NSString stringWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Stylesheet.css" ofType:nil] usedEncoding:nil error:nil];
-  [htmlTemplate setString:stylesheet forKey:@"stylesheet"];
-  [htmlTemplate setString:[Story formatDate:story.date] forKey:@"date"];
-  [htmlTemplate setString:[NSString stringWithFormat:@"%i", story.modelId] forKey:@"storyId"];
-  [htmlTemplate setString:story.body forKey:@"content"];
-  [htmlTemplate setString:story.title forKey:@"storyTitle"];
-  
-  [content loadHTMLString:htmlTemplate.result baseURL:[NSURL URLWithString:baseUrlString]];
-  [htmlTemplate release];  
+    self.title = story.title;
+    
+    // Load up web view content
+    NSString *baseUrlString = [NSString stringWithFormat:@"http://shacknews.com/onearticle.x/%i", story.modelId];
+    
+    if ([[LatestChatty2AppDelegate delegate] isPadDevice]) {
+        [content loadRequest:[NSURLRequest requestWithURLString:baseUrlString]];
+    } else {
+        StringTemplate *htmlTemplate = [[[StringTemplate alloc] initWithTemplateName:@"Story.html"] autorelease];
+        
+        NSString *stylesheet = [NSString stringWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"Stylesheet.css" ofType:nil] usedEncoding:nil error:nil];
+        [htmlTemplate setString:stylesheet forKey:@"stylesheet"];
+        [htmlTemplate setString:[Story formatDate:story.date] forKey:@"date"];
+        [htmlTemplate setString:[NSString stringWithFormat:@"%i", story.modelId] forKey:@"storyId"];
+        [htmlTemplate setString:story.body forKey:@"content"];
+        [htmlTemplate setString:story.title forKey:@"storyTitle"];
+        
+        [content loadHTMLString:htmlTemplate.result baseURL:[NSURL URLWithString:baseUrlString]];
+    }
 }
 
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-  if ([[NSUserDefaults standardUserDefaults] boolForKey:@"landscape"]) return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-  return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    if ([[NSUserDefaults standardUserDefaults] boolForKey:@"landscape"]) return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
 
 - (void)loadChatty {
-  ChattyViewController *viewController = [ChattyViewController chattyControllerWithStoryId:story.modelId];
-  [self.navigationController pushViewController:viewController animated:YES];
+    ChattyViewController *viewController = [ChattyViewController chattyControllerWithStoryId:story.modelId];
+    [self.navigationController pushViewController:viewController animated:YES];
 }
 
 
 
 - (BOOL)webView:(UIWebView *)aWebView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
-  if (navigationType == UIWebViewNavigationTypeLinkClicked) {
-    BrowserViewController *viewController = [[BrowserViewController alloc] initWithRequest:request];
-    [self.navigationController pushViewController:viewController animated:YES];
-    [viewController release];
-    return NO;
-  }
-  
-  return YES;
+    if (navigationType == UIWebViewNavigationTypeLinkClicked) {
+        BrowserViewController *viewController = [[[BrowserViewController alloc] initWithRequest:request] autorelease];
+        [self.navigationController pushViewController:viewController animated:YES];
+        return NO;
+    }
+    
+    return YES;
 }
 
 - (void)dealloc {
-  [storyLoader release];
+    [storyLoader release];
 	NSLog(@"StoryRetainCount: %d", [story retainCount]);
-		  
-  [story release];
-  [super dealloc];
+		    
+    [story release];
+    [super dealloc];
 }
 
 
