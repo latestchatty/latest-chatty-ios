c6a58cf00899c15dce96a6d59cec9f0e735e9b81
Patrick Crager <p.crager@gmail.com>
2012-10-29 22:20:28 -0400

    Added a query string param to ThomW's Shack[lol] so he can do some server-side manipulations instead of client-side via js

--- Classes/RootViewController.m
+++ Classes/RootViewController.m
@@ -159,7 +159,7 @@
             
             //Patch-E: added new menu item for Shack[LOL]-tergration! Passes user= on the URL to allow lol'ing within the web view on the Shack[LOL] site. Uses new BrowserViewController constructor.
         case 4:
-            urlString = [[NSString stringWithFormat:@"http://lol.lmnopc.com?user=%@", [[NSUserDefaults standardUserDefaults] stringForKey:@"username"]] stringByAddingPercentEscapesUsingEncoding:NSASCIIStringEncoding];
+            urlString = [[NSString stringWithFormat:@"http://lol.lmnopc.com?lc_webview=1&user=%@", [[NSUserDefaults standardUserDefaults] stringForKey:@"username"]] stringByAddingPercentEscapesUsingEncoding:NSASCIIStringEncoding];
             viewController = [[[BrowserViewController alloc] initWithRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:urlString]] title:@"Shack[lol]" isForShackLOL:YES] autorelease];
             
             if ([[LatestChatty2AppDelegate delegate] isPadDevice]) {
