bd1c79e142b9e7e112bb03678f34f98ccbea5275
Patrick Crager <p.crager@gmail.com>
2013-05-20 00:05:36 -0400

    Code formatting

--- Classes/Message.m
+++ Classes/Message.m
@@ -19,45 +19,47 @@
 @synthesize unread;
 
 + (ModelLoader *)findAllWithDelegate:(id<ModelLoadingDelegate>)delegate {
-  return [self loadAllFromUrl:@"/messages" delegate:delegate];
+    return [self loadAllFromUrl:@"/messages" delegate:delegate];
 }
 
 + (id)didFinishLoadingPluralData:(id)dataObject {
-  NSArray *modelArray = [dataObject objectForKey:@"messages"];
-  return [super didFinishLoadingPluralData:modelArray];
+    NSArray *modelArray = [dataObject objectForKey:@"messages"];
+    return [super didFinishLoadingPluralData:modelArray];
 }
 
 - (id)initWithDictionary:(NSDictionary *)dictionary {
-  [super initWithDictionary:dictionary];
-  
-  self.from     = [dictionary objectForKey:@"from"]/* stringByUnescapingHTML]*/;
-  self.subject  = [dictionary objectForKey:@"subject"]/* stringByUnescapingHTML]*/;
-  self.body     = [dictionary objectForKey:@"body"];
-  self.date     = [[self class] decodeDate:[dictionary objectForKey:@"date"]];
-  self.unread   = [[dictionary objectForKey:@"unread"] boolValue];
-  
-  return self;
+    [super initWithDictionary:dictionary];
+
+    self.from     = [dictionary objectForKey:@"from"]/* stringByUnescapingHTML]*/;
+    self.subject  = [dictionary objectForKey:@"subject"]/* stringByUnescapingHTML]*/;
+    self.body     = [dictionary objectForKey:@"body"];
+    self.date     = [[self class] decodeDate:[dictionary objectForKey:@"date"]];
+    self.unread   = [[dictionary objectForKey:@"unread"] boolValue];
+
+    return self;
 }
 
 - (NSString *)preview {
-  return [[self.body stringByUnescapingHTML] stringByReplacingOccurrencesOfRegex:@"<.*?>" withString:@""];
+    return [[self.body stringByUnescapingHTML] stringByReplacingOccurrencesOfRegex:@"<.*?>" withString:@""];
 }
 
 - (void)markRead {
-  NSString *string = [Message urlStringWithPath:[NSString stringWithFormat:@"/messages/%i", self.modelId]];
-  NSURL *url = [NSURL URLWithString:string];
-  NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
-  [request setHTTPMethod:@"PUT"];
-  [NSURLConnection connectionWithRequest:request delegate:self];
+    NSString *string = [Message urlStringWithPath:[NSString stringWithFormat:@"/messages/%i", self.modelId]];
+    NSURL *url = [NSURL URLWithString:string];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    [request setHTTPMethod:@"PUT"];
+    [NSURLConnection connectionWithRequest:request delegate:self];
+//    NSString *responseBody = [NSString stringWithData:[NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil]];
+//    NSLog(@"%@",responseBody);
 }
 
 - (void)connection:(NSURLConnection *)connection didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge {
-  if ([challenge previousFailureCount] == 0) {
-    LatestChatty2AppDelegate *appDelegate = (LatestChatty2AppDelegate*)[[UIApplication sharedApplication] delegate];
-    [[challenge sender] useCredential:[appDelegate userCredential] forAuthenticationChallenge:challenge];
-  } else {    
-    [[challenge sender] cancelAuthenticationChallenge:challenge];
-  }
+    if ([challenge previousFailureCount] == 0) {
+        LatestChatty2AppDelegate *appDelegate = (LatestChatty2AppDelegate*)[[UIApplication sharedApplication] delegate];
+        [[challenge sender] useCredential:[appDelegate userCredential] forAuthenticationChallenge:challenge];
+    } else {    
+        [[challenge sender] cancelAuthenticationChallenge:challenge];
+    }
 }
 
 + (BOOL)createWithTo:(NSString *)to subject:(NSString *)subject body:(NSString *)body {
